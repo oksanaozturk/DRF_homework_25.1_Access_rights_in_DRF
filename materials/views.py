3fbf3f2e15dc1763c3ee57676cd37919d9c51023
casper <oksanaozturk@gmail.com>
2024-04-14 22:06:59 +0300

    Создан класс CourseViewSet для реализации CRUD  модели Course

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from materials.models import Course
+from materials.serializer import CourseSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    """Класс для настройки CRUD для модели Course"""
+    serializer_class = CourseSerializer
+    # Получаем все данне из БД
+    queryset = Course.objects.all()
