3cd6dd622aa9f88a7375c8396a4f61e75eeb8a4c
casper <oksanaozturk@gmail.com>
2024-04-14 18:30:39 +0300

    Настройка коды с помощью black

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Класс для регистрации Пользователя в админке."""
+
+    list_display = ("email", "is_active", "password", "avatar", "phone_number")
+    list_filter = ("email",)
+    search_fields = ("email",)
