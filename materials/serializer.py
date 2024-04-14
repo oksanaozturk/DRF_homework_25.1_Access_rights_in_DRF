702c4d423509e7a9b45e64ea15c56731c1a64393
casper <oksanaozturk@gmail.com>
2024-04-14 23:25:26 +0300

    Добавление класса LessonSerializer для модели Lesson

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,11 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
-from materials.models import Course
+from materials.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
-    """Класс для создания сериализатора"""
+    """Класс создания сериализатора для модели Course"""
 
     class Meta:
         model = Course
         fields = "__all__"   # Или кортеж полей, которые необходимо вывести
+
+
+class LessonSerializer(ModelSerializer):
+    """Класс создания сериализатора для модели Lesson"""
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"  # Или кортеж полей, которые необходимо вывести
