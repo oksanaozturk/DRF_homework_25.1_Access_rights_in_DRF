507be5b35a1bc9c81f7a05f5ce2571b8068f389b
casper <oksanaozturk@gmail.com>
2024-04-14 18:29:49 +0300

    Добавление настроев в Админку для моделей Lesson, Course

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Lesson, Course
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Урок в админке."""
+
+    list_display = ("id", "name", "preview", "description", "video")
+    list_filter = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    """Класс для регистрации модели Курс в админке."""
+
+    list_display = ("id", "name", "preview", "description")
+    list_filter = ("name",)
+    search_fields = ("name",)
