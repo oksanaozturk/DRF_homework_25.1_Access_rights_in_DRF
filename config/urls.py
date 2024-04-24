99ecde9b1981bf294b2d25cb3274e045739a0573
casper <oksanaozturk@gmail.com>
2024-04-24 17:57:36 +0300

    Выключены настройки для media

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("materials/", include(("materials.urls", "materials"), namespace="materials")),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
