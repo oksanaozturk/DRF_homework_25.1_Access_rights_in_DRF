a31d05f784b958bbaef2e0ff10b97ad882b465c0
casper <oksanaozturk@gmail.com>
2024-04-29 01:15:47 +0300

    Добавлены: 1) фильтрация -filterset_fields, 2) сортировка - ordering_fields + filter_backends переопределен

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment
@@ -10,3 +12,12 @@ class PaymentViewSet(ModelViewSet):
     serializer_class = PaymentSerializer
     # Получаем все данне из БД
     queryset = Payment.objects.all()
+
+    # Спмсок меняем на кортеж, так как это более защищенная форма данных (указываем то, по чему хотим фильтровать)
+    filterset_fields = ('lesson', 'course', 'payment_method')
+
+    # Настройка сортировки
+    # Переопределяем backends так как для ordering и search нужен filters.OrderingFilter,
+    # который импортируем из rest_framework, а для filterset нужен только DjangoFilterBackend, который из django-filters
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    ordering_fields = ('date_payment',)
