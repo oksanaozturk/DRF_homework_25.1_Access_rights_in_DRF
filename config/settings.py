67be0a8d20afde2763144153ca14e3f821a71ed9
casper <oksanaozturk@gmail.com>
2024-04-29 01:17:22 +0300

    Захештегила django-filters в config/settings.py/INSTALLED_APPS так как выдавало ошибку

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "django-filter"
+    # "django-filters"
     "users",
     "materials",
 ]
