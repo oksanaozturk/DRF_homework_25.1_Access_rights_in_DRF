a82e8ce234eb9d8e313107f47736e2c4f70f677c
casper <oksanaozturk@gmail.com>
2024-04-28 17:45:20 +0300

    добавлены настройки для модели Payment

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from users.models import User
+from users.models import User, Payment
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    """Класс для регистрации Пользователя в админке."""
+    """Класс для регистрации User в админке."""
 
     list_display = ("email", "is_active", "password", "avatar", "phone_number")
     list_filter = ("email",)
     search_fields = ("email",)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    """Класс для регистрации и отображения модели Payment в админке."""
+
+    list_display = ("id", "user", "date_payment", "course", "lesson", "amount", "payment_method")
+    list_filter = ("user",)
