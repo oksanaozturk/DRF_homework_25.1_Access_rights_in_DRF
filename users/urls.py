fc2ee9db47b88fa1252a4357a8e833fa7fa8a62e
casper <oksanaozturk@gmail.com>
2024-05-29 22:07:26 +0300

    1) Добавлены пути для реализации CRUD для модели User 2) Добавлены пути для реализации JWT-авторизации - "login/" и 'token/refresh/'

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentViewSet
+from users.views import PaymentViewSet, UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, \
+    UserDestroyAPIView
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name = UsersConfig.name
 
@@ -12,6 +15,19 @@ router = SimpleRouter()
 router.register("payments", PaymentViewSet)
 
 urlpatterns = [
+    # Путь для вывода страницы со всеми объектами модели User
+    path("users-list/", UserListAPIView.as_view(), name="users-list"),
+    # Путь для вывода страницы с одним объектом модели User
+    path("user-view/<int:pk>/", UserRetrieveAPIView.as_view(), name="users-retrieve"),
+    # Путь для вывода страницы при создании нового объекта модели User
+    path("user-create/", UserCreateAPIView.as_view(), name="users-create"),
+    # Путь для редактирования объекта модели User
+    path("user-update/<int:pk>/", UserUpdateAPIView.as_view(), name="users-update"),
+    # Путь для удаления объекта модели User
+    path("user-destroy/<int:pk>/", UserDestroyAPIView.as_view(), name="users-destroy"),
+
+    path("login/", TokenObtainPairView.as_view(), name='login'),
+    path("token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
 
     ] + router.urls
 
