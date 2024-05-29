89660f82a2ae4d73f6787544a06ccd282c7655b7
casper <oksanaozturk@gmail.com>
2024-05-29 22:00:35 +0300

    Добавлен класс сериализатора для модели User -  UserSerializer

--- users/serializer.py
+++ users/serializer.py
@@ -1,11 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
-from users.models import Payment
+from users.models import Payment, User
 
 
 class PaymentSerializer(ModelSerializer):
-    """Класс создания скриализатора для модели Payment"""
+    """Класс создания сериализатора для модели Payment"""
 
     class Meta:
         model = Payment
+        fields = "__all__"  # Или кортеж полей, которые необходимо вывести
+
+
+class UserSerializer(ModelSerializer):
+    """Класс создания сериализатора для модели User"""
+
+    class Meta:
+        model = User
         fields = "__all__"  # Или кортеж полей, которые необходимо вывести
