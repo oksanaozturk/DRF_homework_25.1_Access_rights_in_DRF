1ef06ae033d20f85655f4a9740e623c6886e5224
casper <oksanaozturk@gmail.com>
2024-05-29 22:11:13 +0300

    1) Добавлены классы для реализации CRUD для модели User - UserCreateAPIView, UserUpdateAPIView, UserDestroyAPIView, UserListAPIView, UserRetrieveAPIView 2) Добавлен метод perform_create в класс UserCreateAPIView для предотвращения конфликта из-за ранее заданной 'username = None',         указанная нами в модели User в models.py

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from users.models import Payment
-from users.serializer import PaymentSerializer
+from users.models import Payment, User
+from users.serializer import PaymentSerializer, UserSerializer
 
 
 class PaymentViewSet(ModelViewSet):
@@ -21,3 +22,42 @@ class PaymentViewSet(ModelViewSet):
     # который импортируем из rest_framework, а для filterset нужен только DjangoFilterBackend, который из django-filters
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     ordering_fields = ('date_payment',)
+
+
+class UserCreateAPIView(CreateAPIView):
+    """Класс для создания экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        """Данный метод необходим для того, чтобы нам не мешала настройка 'username = None',
+        указанная нами в модели User в models.py"""
+        user = serializer.save(is_active=True)
+        # Кешируем обязательно пароль Пользователя
+        user.set_password(user.password)
+        user.save()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """Класс для редактирования экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    # Получаем все данне из БД
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    """Класс для удаления экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserListAPIView(ListAPIView):
+    """Класс для выведения всех экземпляров модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """Класс для выведения одного экземпляра модели User (CRUD)"""
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
