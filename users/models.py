282cd5fc2c53735e24feca4b0d1272bb94196526
casper <oksanaozturk@gmail.com>
2024-06-03 06:49:56 +0300

    Добавление полей в модель Payment - session_id и link

--- users/models.py
+++ users/models.py
@@ -62,6 +62,11 @@ class Payment(models.Model):
                                verbose_name="Оплаченный курс", null=True, blank=True)
     lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, related_name="payments",
                                verbose_name="Оплаченный урок", null=True, blank=True)
-    amount = models.PositiveIntegerField(verbose_name="Сумма оплаты")
+    amount = models.PositiveIntegerField(verbose_name="Сумма оплаты", help_text='Укажите сумму платежа',)
     payment_method = models.CharField(max_length=50, choices=CHOICES_PAYMENT_METHOD, verbose_name="Способ оплаты",
                                       null=True, blank=True)
+    session_id = models.CharField(max_length=255, verbose_name="ID сессии",
+                                  help_text='Укажите ID Сессии', **NULLABLE)
+    # ССылка длинная, поэтому символов даем больше
+    link = models.URLField(max_length=400, verbose_name="Ссылка на оплату",
+                           help_text='Укажите ссылку на оплату', **NULLABLE)
