bf9a2ec8dabde36f97ff4bd3823d71b75b2d677b
casper <oksanaozturk@gmail.com>
2024-06-06 20:27:04 +0300

    Редактирование функции send_message_about_update_course

--- materials/tasks.py
+++ materials/tasks.py
@@ -1,5 +1,5 @@
-from django.core.mail import send_mail
 from celery import shared_task
+from django.core.mail import send_mail
 
 from config import settings
 from materials.models import Course, SubscriptionCourse
@@ -13,16 +13,19 @@ def send_message_about_update_course(course_id):
     print("Отправка письма ...")
     course = Course.objects.get(pk=course_id)
     course_users = SubscriptionCourse.objects.filter(course=course_id)
+    # При условии, что course_users у нас не пустой будет формироваться email_list
     email_list = []
     for user in course_users:
         email_list.append(user.email)
+    # При условии, что email_list у нас не пустой будет производится отправка писем
+    if email_list:
         send_mail(
             subject=f'Обновление по курсу {course.name}',
             message=f'Вы подписаны на обновления курса, вышла новая информция. Рекомендуем Вас ознакомиться с ней"',
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=email_list,
             fail_silently=True
-        )
+            )
 # #
 # @shared_task
 # def send_message_for_update(course_id):
